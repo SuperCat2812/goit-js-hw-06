8a40e8b01933b7ebcd8fae612df30738b0de9baf
SuperCat2812 <64689542+SuperCat2812@users.noreply.github.com>
2026-04-10 18:33:54 +0200

    Refactor task files to implement customer and storage classes with order management

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,25 @@
-function getElementWidth(content, padding, border) {
-  return parseFloat(content) + (parseFloat(padding) + parseFloat(border)) * 2;
+class StringBuilder {
+  constructor(value) {
+    this.value = value;
+  }
+  getValue() {
+    return this.value;
+  }
+  padStart(value) {
+    this.value = value + this.value;
+  }
+  padEnd(value) {
+    this.value += value;
+  }
+  padBoth(value) {
+    this.value = value + this.value + value;
+  }
 }
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
